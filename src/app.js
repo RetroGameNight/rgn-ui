9000f6b73ca8f1639a013ff2348eb2ed63233593
Sasha Fahrenkopf <sfahrenk@gmail.com>
2015-07-02 12:02:19 -0700

    Fixed reference error to AppActions.js

--- src/app.js
+++ src/app.js
@@ -50,6 +50,5 @@ Promise.all([
     } else {
       window.attachEvent('onload', resolve);
     }
-  }).then(() => FastClick.attach(document.body)),
-  new Promise((resolve) => AppActions.loadPage(path, resolve))
+  }).then(() => FastClick.attach(document.body))
 ]).then(run);
