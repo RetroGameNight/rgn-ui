7d155e436df30de062a435ec71bf6ba1f2b9d0b6
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-07-09 02:58:09 -0700

    Added basic logic to Grid component

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -5,12 +5,29 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
-import './App.less';
+import './Grid.less';
 import React, { PropTypes } from 'react';
+import _ from 'underscore'
 
 export default class Grid extends React.Component {
 	render() {
+		const headers = _.chain(this.props.headers)
+			.map((eachRow) => {
+				const row = _.chain(eachRow)
+					.map((eachElement) => <td>{eachElement}</td>)
+				return <tr>{row}</tr>
+			})
+		const rows = _.chain(this.props.rows)
+			.map((eachRow) => {
+				const row = _.chain(eachRow)
+					.map((eachElement) => <td>{eachElement}</td>)
+				return <tr>{row}</tr>
+			})
 		return (
-		);
+			<table>
+				<thead>{headers}</thead>
+				<tbody>{rows}</tbody>
+			</table>
+		)
 	}
 }
