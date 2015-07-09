eb26d9b27ad179038cc308466cd9a7c77ead162c
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-07-09 02:37:03 -0700

    Added template Grid component

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,16 @@
+/*
+ * Retro Game Night
+ * Copyright (c) 2015 Sasha Fahrenkopf, ... add your name
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+import './App.less';
+import React, { PropTypes } from 'react';
+
+export default class Grid extends React.Component {
+	render() {
+		return (
+		);
+	}
+}
