713d9997cc2337f345a9abb35f8b59ef34f63b3c
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-07-13 11:56:10 -0700

    Made a more configurable modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,25 +5,34 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
-import './Modal.less';
-import React, { PropTypes } from 'react';
+import './Modal.less'
+import React from 'react'
 
 export default class Modal extends React.Component {
   constructor(props) {
     super(props)
   }
-  modalShouldBeClosed() {
+  modalIsClosed() {
     return !this.props.isOpen
   }
   render() {
-    if (this.modalShouldBeClosed()) {
-       return (<div></div>)
+    const handleCancelButtonClick = this.props.handleCancelButtonClick
+    const handleSubmitButtonClick = this.props.handleSubmitButtonClick
+    if (this.modalIsClosed()) {
+      return (<div></div>)
     } else {
       return (
         <div className="Modal">
           <div className="modal-dialog">
             <div className="modal-content">
-              {this.props.children}
+              <ModalHeader 
+                  handleClick={handleCancelButtonClick}/>
+              <ModalContent>
+                {this.props.children}
+              </ModalContent>
+              <ModalFooter 
+                  handleCancelButtonClick={handleCancelButtonClick}
+                  handleSubmitButtonClick={handleSubmitButtonClick}/>
             </div>
           </div>
         </div>
@@ -31,3 +40,51 @@ export default class Modal extends React.Component {
     }
   }
 }
+
+class ModalHeader extends React.Component {
+  render() {
+    const title = 
+      this.props.title && <h4 className="modal-title">{this.props.title}</h4>
+    return (
+      <div className="modal-header">
+        <button type="button" class="close"
+                ref="modalCloseButton"
+                onClick={this.props.handleClick}>
+          <span>&times;</span>
+        </button>
+        {title}
+      </div>
+    ) 
+  }
+}
+
+class ModalContent extends React.Component {
+  render() {
+    return (
+      <div className="modal-body">
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+class ModalFooter extends React.Component {
+  render() {
+    const cancelButtonText = this.props.cancelButtonText || "Cancel"
+    const submitButtonText = this.props.submitButtonText || "Submit"
+    return (
+      <div className="modal-footer">
+        <button type="button" class="btn btn-default"
+                onClick={this.handleCancelButtonClick}>
+          {cancelButtonText}
+        </button>
+        <button type="button" class="btn btn-primary">
+                onClick={this.handleSubmitButtonClick}>
+          {submitButtonText}
+        </button>
+      </div>
+    )
+  }
+}
+
+
