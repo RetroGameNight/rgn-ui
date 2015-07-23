baa8a10a2d4f89a4528fc0a253429e24c6b2937e
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-07-23 12:53:37 -0700

    Modal Component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -16,75 +16,17 @@ export default class Modal extends React.Component {
     return !this.props.isOpen
   }
   render() {
-    const handleCancelButtonClick = this.props.handleCancelButtonClick
-    const handleSubmitButtonClick = this.props.handleSubmitButtonClick
     if (this.modalIsClosed()) {
       return (<div></div>)
     } else {
       return (
-        <div className="Modal">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <ModalHeader 
-                  handleClick={handleCancelButtonClick}/>
-              <ModalContent>
-                {this.props.children}
-              </ModalContent>
-              <ModalFooter 
-                  handleCancelButtonClick={handleCancelButtonClick}
-                  handleSubmitButtonClick={handleSubmitButtonClick}/>
-            </div>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            {this.props.children}
           </div>
         </div>
       )
     }
   }
 }
-
-class ModalHeader extends React.Component {
-  render() {
-    const title = 
-      this.props.title && <h4 className="modal-title">{this.props.title}</h4>
-    return (
-      <div className="modal-header">
-        <button type="button" class="close"
-                ref="modalCloseButton"
-                onClick={this.props.handleClick}>
-          <span>&times;</span>
-        </button>
-        {title}
-      </div>
-    ) 
-  }
-}
-
-class ModalContent extends React.Component {
-  render() {
-    return (
-      <div className="modal-body">
-        {this.props.children}
-      </div>
-    )
-  }
-}
-
-class ModalFooter extends React.Component {
-  render() {
-    const cancelButtonText = this.props.cancelButtonText || "Cancel"
-    const submitButtonText = this.props.submitButtonText || "Submit"
-    return (
-      <div className="modal-footer">
-        <button type="button" class="btn btn-default"
-                onClick={this.handleCancelButtonClick}>
-          {cancelButtonText}
-        </button>
-        <button type="button" class="btn btn-primary">
-                onClick={this.handleSubmitButtonClick}>
-          {submitButtonText}
-        </button>
-      </div>
-    )
-  }
-}
-
 
