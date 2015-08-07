5cfbc0b06b715c17b707d94a045fbe2de6265445
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-08-07 13:27:48 -0700

    Readded removed React import

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
+import React from 'react'
 import App from './components/App'
 import NotFound from './components/NotFound'
 import ChallengePage from './components/ChallengePage'
