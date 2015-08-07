95686d75e195f19c1f5bb67e6546ce5e1c41dfc8
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-08-07 14:42:16 -0700

    Fixed some linting error

--- src/server.js
+++ src/server.js
@@ -14,16 +14,16 @@ app.set('port', config.server.location.port)
 
 app.get('/', (req, res) => {
   res.sendfile('build/index.html')
-});
+})
 
 app.get('/app.js', (req, res) => {
   res.sendfile('build/app.js')
-});
+})
 
-app.use('/assets', express.static('build/assets'));
-app.use('/css', express.static('build/css'));
-app.use('/fonts', express.static('build/fonts'));
+app.use('/assets', express.static('build/assets'))
+app.use('/css', express.static('build/css'))
+app.use('/fonts', express.static('build/fonts'))
 
-const server = app.listen(app.get('port'), () => {
-  console.log('rgn-ui listening on port %s', app.get('port'));
-});
+app.listen(app.get('port'), () => {
+  console.log('rgn-ui listening on port %s', app.get('port'))
+})
