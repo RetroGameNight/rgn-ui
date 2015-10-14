166c89f3d61bba7772c9af202f7a39bffbca75a8
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-10-13 22:51:15 -0700

    Added profile editor to user settings
    Something is fucked up

--- src/components/UserSettingsPage/UserSettingsPage.js
+++ src/components/UserSettingsPage/UserSettingsPage.js
@@ -13,7 +13,7 @@ import Page from '../Page'
 
 export default class UserSettingsPage extends React.Component {
   componentDidMount() {
-    flux.getActions('api').getEvents()
+    flux.getActions('api').login()
   }
   render() {
     return (
@@ -40,10 +40,56 @@ class UserSettingsPageInner extends React.Component {
     const id = activeUser ? activeUser.id : ''
     return (
       <Page>
+        <PublicProfilePanel activeUser={activeUser}/>
         <DeleteAccountPanel id={id}/>
       </Page>
     )
   }
+}
+
+class PublicProfilePanel extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: props.activeUser ? props.activeUser.name : '',
+      email: props.activeUser ? props.activeUser.email : '',
+    }
+  }
+  handleChange = (event) => {
+    this.setState({[event.target.id]: event.target.value})
+  }
+  handleClick = () => {
+    const { id } = this.props.activeUser
+    flux.getActions('api').updateUser(id, this.state)
+  }
+  render() {
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">Public profile</div>
+          <div className="panel-body">
+            <form>
+              <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" className="form-control" id="name"
+                    onChange={this.handleChange}
+                    value={this.state.name}/>
+              </div>
+              <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" className="form-control" id="email"
+                    onChange={this.handleChange}
+                    value={this.state.email}/>
+              </div>
+              <button type="submit" className="btn btn-default"
+                  onClick={this.handleClick}>Update</button>
+            </form>
+          </div>
+      </div>
+    )
+  }
+}
+PublicProfilePanel.propTypes = {
+  activeUser: PropTypes.object.isRequired
 }
 
 class DeleteAccountPanel extends React.Component {
